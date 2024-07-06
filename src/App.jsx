e35b53e0f8896cc5d51c19bda90edfa54fcd1c5a
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-07-06 22:08:15 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelpChatBot from './components/HelpChatBot';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <HelpChatBot />
+    </div>
+  );
+}
+
+export default App;
